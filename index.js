212137c8021384a9300d0c1c2521c83906ca2379
rob <brubachr@gmail.com>
2022-08-16 11:48:38 -0700

    changed slide out menu to 70%

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /* Open when someone clicks on the span element */
 function openNav() {
-  document.getElementById("myNav").style.width = "60%";
+  document.getElementById("myNav").style.width = "70%";
 }
 
 /* Close when someone clicks on the "x" symbol inside the overlay */
