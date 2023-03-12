0059893ecd21459cd921c1682bf56c8dfdde8af6
rob <brubachr@gmail.com>
2023-03-11 23:17:43 -0700

    added progav and wallflex

--- index.js
+++ index.js
@@ -789,8 +789,17 @@ alert("NOT MR-CONDITIONAL");
 
   }else if(pacerValue.toUpperCase() === 'STRATA'|| pacerValue.toUpperCase() === 'STRATAMR'){
     alert("Page 12 is MRI Safety information") ;
-   window.open('https://manuals.medtronic.com/content/dam/emanuals/st/CONTRIB_249312.pdf', '_blank');   
+   window.open('https://manuals.medtronic.com/content/dam/emanuals/st/CONTRIB_249312.pdf', '_blank');  
+   
+  }else if(pacerValue.toUpperCase() === 'ADVANCED BIONICS'|| pacerValue.toUpperCase() === 'HIRES ULTRA 3D' || pacerValue.toUpperCase() === 'CI-1601-04'){
+   window.open('https://www.advancedbionics.com/content/dam/advancedbionics/Documents/Regional/en-us/Professionals/hires-ultra-3d-mri-safety-information.pdf', '_blank'); 
 
+  }else if(pacerValue.toUpperCase() === 'WALLFLEX'|| pacerValue.toUpperCase() === 'COLONIC AND DUODENAL SOFT STENT'){
+   window.open('https://www.bostonscientific.com/content/dam/bostonscientific/endo/portfolio-group/wallflex-colonic-and-duodenal/Wallflex_Soft_Abbreviated_DFU_FINAL.pdf', '_blank');  
+
+
+  }else if(pacerValue.toUpperCase() === 'PROGAV 2.0'|| pacerValue.toUpperCase() === 'PROGAV'){
+   window.open('Docs/proGav.pdf', '_blank');  
   
 
 
