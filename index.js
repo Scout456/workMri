e21ab7af3401aa0914700ab507e6eb2702588fd4
rob <brubachr@gmail.com>
2023-09-04 21:02:49 -0700

    added <br> in html

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ document.getElementById("myNav").style.width = "70%";
 function openNav(){
    var nav = document.getElementById('myNav');
    if(window.innerWidth > 700){
-      nav.style.width="45%";
+      nav.style.width="40%";
    }else{
       window.innerWidth < 700;
       nav.style.width="80%";
